e49cd6c4c3f236abbdfe5de7e056faac32287696
zayan93 <zayan_93@mail.ru>
2025-06-17 13:16:28 +0300

    flags added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,14 +3,37 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"go-url-shortner/internal/app"
+	"go-url-shortner/internal/config"
 	"log"
 	"net/http"
+	"net/url"
 )
 
+func BaseURLMiddleware(baseURL string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			parsedURL, err := url.Parse(baseURL)
+			if err == nil {
+				r.Host = parsedURL.Host
+			} else {
+				r.Host = baseURL
+			}
+			next.ServeHTTP(w, r)
+
+		})
+	}
+}
+
 func main() {
+	cfg := config.New()
+
 	r := chi.NewRouter()
+
+	// Добавляю baseurl в middleware чтобы не трогать логику postpage хендлера и не менять тесты
+
+	r.Use(BaseURLMiddleware(cfg.BaseURL))
 	r.Post("/", app.PostPage)
 	r.Get("/{id}", app.GetPage)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(cfg.Address, r))
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,2 +1,22 @@
 package config
 
+import (
+	"flag"
+)
+
+type Config struct {
+	Address string // адрес запуска HTTP-сервера, например localhost:8080
+	BaseURL string // базовый URL для сокращённых ссылок, например http://localhost:8080
+}
+
+// New создает и инициализирует конфигурацию из флагов командной строки
+func New() *Config {
+	addr := flag.String("a", "localhost:8080", "HTTP server address")
+	baseURL := flag.String("b", "http://localhost:8080", "Base URL for short links")
+	flag.Parse()
+
+	return &Config{
+		Address: *addr,
+		BaseURL: *baseURL,
+	}
+}
