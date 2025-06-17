99f682296a75bbdab093c61d86980c0c03858896
zayan93 <zayan_93@mail.ru>
2025-06-17 12:46:28 +0300

    added chi framework

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"go-url-shortner/internal/app"
+	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.PostPage)
-	mux.HandleFunc("/{id}", app.GetPage)
+	r := chi.NewRouter()
+	r.Post("/", app.PostPage)
+	r.Get("/{id}", app.GetPage)
 
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
