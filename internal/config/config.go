88c81bbcc3edf78d078fae96b331eb31394eca82
zayan93 <zayan_93@mail.ru>
2025-06-26 16:10:08 +0300

    last modified

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,9 +6,10 @@ import (
 )
 
 type Config struct {
-	Address  string // адрес запуска HTTP-сервера, например localhost:8080
-	BaseURL  string // базовый URL для сокращённых ссылок, например http://localhost:8080
-	LogLevel string // Уровень логирования
+	Address         string // адрес запуска HTTP-сервера, например localhost:8080
+	BaseURL         string // базовый URL для сокращённых ссылок, например http://localhost:8080
+	LogLevel        string // Уровень логирования
+	FileStoragePath string // путь до файла с данными
 }
 
 // New создает и инициализирует конфигурацию из флагов командной строки
@@ -16,10 +17,12 @@ func New() *Config {
 	defaultAddress := "localhost:8080"
 	defaultBaseURL := "http://localhost:8080"
 	defaultLogLevel := "info"
+	defaultFileStoragePath := "store/storage.txt"
 
 	envAddress := os.Getenv("SERVER_ADDRESS")
 	envBaseURL := os.Getenv("BASE_URL")
 	envLogLevel := os.Getenv("LOG_LEVEL")
+	envFileStoragePath := os.Getenv("FILE_STORAGE_PATH")
 
 	if envAddress == "" {
 		envAddress = defaultAddress
@@ -32,14 +35,20 @@ func New() *Config {
 		envLogLevel = defaultLogLevel
 	}
 
+	if envFileStoragePath == "" {
+		envFileStoragePath = defaultFileStoragePath
+	}
+
 	addr := flag.String("a", envAddress, "HTTP server address")
 	baseURL := flag.String("b", envBaseURL, "Base URL for short links")
 	logLevel := flag.String("l", envLogLevel, "Log level")
+	fileStoragePath := flag.String("f", envFileStoragePath, "File storage path")
 	flag.Parse()
 
 	return &Config{
-		Address:  *addr,
-		BaseURL:  *baseURL,
-		LogLevel: *logLevel,
+		Address:         *addr,
+		BaseURL:         *baseURL,
+		LogLevel:        *logLevel,
+		FileStoragePath: *fileStoragePath,
 	}
 }
